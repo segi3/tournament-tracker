eb9cafd81212a9708c83a466574c9132c25b9ab8
Rafi Nizar Abiyyi <rafi.nizar15@gmail.com>
2022-10-27 20:32:08 +0700

    data models

---
+++ TrackerLibrary/PersonModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    /// <summary>
+    /// Represents on person
+    /// </summary>
+    public class PersonModel
+    {
+        /// <summary>
+        /// The first name of the person
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The last name of the person
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The primary email address of the person
+        /// </summary>
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// The primary cell phone number of the person
+        /// </summary>
+        public string CellphoneNumber { get; set; }
+    }
+}
